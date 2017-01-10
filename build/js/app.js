0a8460bd904751e517ae64002096d56771d86613
Annie <nsab42@yahoo.com>
2017-01-10 09:59:39 -0800

    fixed the .gitignore

--- build/js/app.js
+++ build/js/app.js
@@ -9,7 +9,7 @@ function Doctor() {
 }
 
 Doctor.prototype.getDoctorsBioByIssue = function(medicalIssue, displayFunction) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue +'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey).then(function(response) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue +'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=40&user_key=' + apiKey).then(function(response) {
     console.log(response);
     displayFunction(medicalIssue, response.data);
   }).fail(function(error) {
